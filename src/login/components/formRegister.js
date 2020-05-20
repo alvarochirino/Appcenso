0373d6e5549be41c6805834de53d33edc3c59edb
Christian-martinez <christ96mart@gmail.com>
2020-05-19 21:08:05 -0400

    arreglos en ios y observaciones

--- src/login/components/formRegister.js
+++ src/login/components/formRegister.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   TextInput,
@@ -7,60 +7,61 @@ import {
   TouchableOpacity,
   Alert,
   ActivityIndicator,
-} from 'react-native';
-import {StackActions, NavigationActions} from 'react-navigation';
-import AsyncStorage from '@react-native-community/async-storage';
+} from "react-native";
+import { StackActions, NavigationActions } from "react-navigation";
+import AsyncStorage from "@react-native-community/async-storage";
 
-import API from '../../../utils/api';
-import Dropdown from '../../modules/estudioSocioEcon/components/dropDown';
+import API from "../../../utils/api";
+import Dropdown from "../../modules/estudioSocioEcon/components/dropDown";
 export default class FormRegister extends Component {
   state = {
-    nombre1: '',
-    nombre2: '',
-    apellido1: '',
-    apellido2: '',
-    doc_tipo: '',
-    doc_numero: '',
-    doc_exp: '',
-    email: '',
-    password: '',
+    nombre1: "",
+    nombre2: "",
+    apellido1: "",
+    apellido2: "",
+    doc_tipo: "",
+    doc_numero: "",
+    doc_exp: "",
+    email: null,
+    password: "",
     emailValido: false,
-    password2: '',
+    password2: "",
     mostrarBoton: true,
     tipoDoc: [
-      {id: 'CE', value: 'CE'},
-      {id: 'CI', value: 'Carnet de Identidad'},
-      {id: 'PAS', value: 'PAS'},
+      { id: "CE", value: "Carnet de Extranjero" },
+      { id: "CI", value: "Carnet de Identidad" },
+      { id: "PAS", value: "Pasaporte" },
     ],
     expDoc: [
-      {value: 'BN'},
-      {value: 'CB'},
-      {value: 'CH'},
-      {value: 'LP'},
-      {value: 'OR'},
-      {value: 'PD'},
-      {value: 'PT'},
-      {value: 'SC'},
-      {value: 'TJ'},
-      {value: 'EX'},
+      { value: "BN" },
+      { value: "CB" },
+      { value: "CH" },
+      { value: "LP" },
+      { value: "OR" },
+      { value: "PD" },
+      { value: "PT" },
+      { value: "SC" },
+      { value: "TJ" },
+      { value: "EX" },
     ],
   };
 
-  validate (text) {
+  validate(text) {
     email = /^[^@]+@[^@]+\.[a-zA-Z]{2,4}$/;
-    if (email.test (text)) {
-      this.setState ({
+    if (email.test(text)) {
+      this.setState({
         email: text,
         emailValido: true,
       });
     } else {
-      this.setState ({
+      this.setState({
         emailValido: false,
       });
     }
   }
 
   _registrar = () => {
+    console.log(this.state);
     const {
       nombre1,
       nombre2,
@@ -73,21 +74,21 @@ export default class FormRegister extends Component {
       password,
       password2,
     } = this.state;
-    if (nombre1 === '') {
-      Alert.alert ('Ingrese nombre');
-    } else if (apellido1 === '') {
-      Alert.alert ('Ingrese apellido paterno');
-    } else if (doc_tipo === '') {
-      Alert.alert ('Seleccione un tipo de documento');
-    } else if (doc_numero === '') {
-      Alert.alert ('Ingrese número de documento');
+    if (nombre1 === "") {
+      Alert.alert("Ingrese nombre");
+    } else if (apellido1 === "") {
+      Alert.alert("Ingrese primer apellido");
+    } else if (doc_tipo === "") {
+      Alert.alert("Seleccione un tipo de documento");
+    } else if (doc_numero === "") {
+      Alert.alert("Ingrese número de documento");
     } else if (password.length < 6) {
-      Alert.alert ('La contraseña debe tener un mínimo de 6 caracteres');
+      Alert.alert("La contraseña debe tener un mínimo de 6 caracteres");
     } else if (password !== password2) {
-      Alert.alert ('las passwords no coinciden');
+      Alert.alert("las passwords no coinciden");
     } else {
-      this.setState ({mostrarBoton: false});
-      API.registroUsuario (
+      this.setState({ mostrarBoton: false });
+      API.registroUsuario(
         nombre1,
         nombre2,
         apellido1,
@@ -97,49 +98,50 @@ export default class FormRegister extends Component {
         doc_numero,
         email,
         password
-      ).then (responseData => {
+      ).then((responseData) => {
         if (responseData != null) {
-          if (responseData == 'The given data was invalid.') {
-            this.setState ({mostrarBoton: true});
-            Alert.alert (
-              'Número de documento y/o correo de usuario ya existente'
-            );
-          } else if (responseData.message == 'usuario creado correctamente') {
-            Alert.alert ('Registro realizado correctamente');
-            API.login (
-              this.state.doc_numero,
-              this.state.password
-            ).then (responseData => {
-              console.log ('responseData', responseData);
-              if (responseData.access_token != null) {
-                const id = responseData.user.id.toString ();
-                try {
-                  AsyncStorage.setItem (
-                    '@User:token',
-                    responseData.access_token
-                  );
-                  AsyncStorage.setItem ('@User:id', id);
-                } catch (e) {
-                  console.error ('@user', e.error);
+          if (responseData == "The given data was invalid.") {
+            this.setState({ mostrarBoton: true });
+            Alert.alert("Número de documento ya existente");
+          } else if (responseData.message == "usuario creado correctamente") {
+            Alert.alert("Registro realizado correctamente");
+            API.login(this.state.doc_numero, this.state.password).then(
+              (responseData) => {
+                console.log("responseData", responseData);
+                if (responseData.access_token != null) {
+                  const id = responseData.user.id.toString();
+                  try {
+                    AsyncStorage.setItem(
+                      "@User:token",
+                      responseData.access_token
+                    );
+                    AsyncStorage.setItem("@User:id", id);
+                  } catch (e) {
+                    console.error("@user", e.error);
+                  }
+                  console.log("NewToken", AsyncStorage.getItem("@User:token"));
+                  console.log("User:id", AsyncStorage.getItem("@User:id"));
+                  const resetAction = StackActions.reset({
+                    index: 0,
+                    actions: [
+                      NavigationActions.navigate({ routeName: "Home" }),
+                    ],
+                  });
+                  this.props.navigation.dispatch(resetAction);
+                } else {
+                  const resetAction = StackActions.reset({
+                    index: 0,
+                    actions: [
+                      NavigationActions.navigate({ routeName: "Login" }),
+                    ],
+                  });
+                  this.props.navigation.dispatch(resetAction);
                 }
-                console.log ('NewToken', AsyncStorage.getItem ('@User:token'));
-                console.log ('User:id', AsyncStorage.getItem ('@User:id'));
-                const resetAction = StackActions.reset ({
-                  index: 0,
-                  actions: [NavigationActions.navigate ({routeName: 'Home'})],
-                });
-                this.props.navigation.dispatch (resetAction);
-              } else {
-                const resetAction = StackActions.reset ({
-                  index: 0,
-                  actions: [NavigationActions.navigate ({routeName: 'Login'})],
-                });
-                this.props.navigation.dispatch (resetAction);
               }
-            });
+            );
           } else {
-            this.setState ({mostrarBoton: true});
-            Alert.alert ('Ocurrio un error, vuelva a intentar');
+            this.setState({ mostrarBoton: true });
+            Alert.alert("Ocurrio un error, vuelva a intentar");
           }
         }
       });
@@ -147,64 +149,64 @@ export default class FormRegister extends Component {
   };
 
   elegirTipoDoc = (value, index) => {
-    this.setState ({
+    this.setState({
       doc_tipo: this.state.tipoDoc[index].id,
     });
   };
 
   elegirExp = (value, index) => {
-    this.setState ({
+    this.setState({
       doc_exp: this.state.expDoc[index].value,
     });
   };
 
-  render () {
-    const {tipoDoc, expDoc} = this.state;
+  render() {
+    const { tipoDoc, expDoc } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.row}>
           <Text style={styles.text}>PRIMER NOMBRE:</Text>
           <TextInput
             style={styles.input}
-            maxLength= {20}
-            onChangeText={text => this.setState ({nombre1: text})}
+            maxLength={20}
+            onChangeText={(text) => this.setState({ nombre1: text })}
           />
         </View>
         <View style={styles.row}>
           <Text style={styles.text}>SEGUNDO NOMBRE:</Text>
           <TextInput
             style={styles.input}
-            maxLength= {20}
-            onChangeText={text => this.setState ({nombre2: text})}
+            maxLength={20}
+            onChangeText={(text) => this.setState({ nombre2: text })}
           />
         </View>
         <View style={styles.row}>
           <Text style={styles.text}>PRIMER APELLIDO:</Text>
           <TextInput
             style={styles.input}
-            maxLength= {20}
-            onChangeText={text => this.setState ({apellido1: text})}
+            maxLength={20}
+            onChangeText={(text) => this.setState({ apellido1: text })}
           />
         </View>
         <View style={styles.row}>
           <Text style={styles.text}>SEGUNDO APELLIDO:</Text>
           <TextInput
             style={styles.input}
-            maxLength= {20}
-            onChangeText={text => this.setState ({apellido2: text})}
+            maxLength={20}
+            onChangeText={(text) => this.setState({ apellido2: text })}
           />
         </View>
-        <View style={[styles.row, {margin: 0}]}>
-          <View style={styles.dropdown}>
+        <View style={[styles.row, { margin: 0 }]}>
+          <View style={styles.dropdown1}>
             <Dropdown
               label="TIPO DE DOCUMENTO"
               data={tipoDoc}
               onChangeText={this.elegirTipoDoc}
             />
           </View>
-          <View style={styles.dropdown}>
+          <View style={styles.dropdown2}>
             <Dropdown
-              label="EXPEDIDO EN"
+              label="EXP. EN"
               data={expDoc}
               onChangeText={this.elegirExp}
             />
@@ -214,16 +216,16 @@ export default class FormRegister extends Component {
           <Text style={styles.text}>Nº DOCUMENTO:</Text>
           <TextInput
             style={styles.input}
-            maxLength= {15}
-            onChangeText={text => this.setState ({doc_numero: text})}
+            maxLength={15}
+            onChangeText={(text) => this.setState({ doc_numero: text })}
           />
         </View>
         <View style={styles.row}>
           <Text style={styles.text}>CORREO ELECTRONICO:</Text>
           <TextInput
             style={styles.input}
-            onChangeText={text => this.validate (text)}
-            keyboardType={'email-address'}
+            onChangeText={(text) => this.validate(text)}
+            keyboardType={"email-address"}
           />
         </View>
         <View style={styles.row}>
@@ -231,59 +233,61 @@ export default class FormRegister extends Component {
           <TextInput
             style={styles.input}
             secureTextEntry={true}
-            onChangeText={text => this.setState ({password: text})}
+            onChangeText={(text) => this.setState({ password: text })}
           />
         </View>
         <View style={styles.row}>
-          <View style={{flexDirection: 'column'}}>
+          <View style={{ flexDirection: "column" }}>
             <Text style={styles.txt1}>CONFIRMACION DE</Text>
             <Text style={styles.text}>CONTRASEÑA:</Text>
           </View>
           <TextInput
             style={styles.input}
             secureTextEntry={true}
-            onChangeText={text => this.setState ({password2: text})}
+            onChangeText={(text) => this.setState({ password2: text })}
           />
         </View>
-        {this.state.mostrarBoton
-          ? <TouchableOpacity onPress={this._registrar} style={styles.button}>
-              <Text style={styles.buttonText}>REGISTRAR</Text>
-            </TouchableOpacity>
-          : <ActivityIndicator style={{margin: 14}} />}
+        {this.state.mostrarBoton ? (
+          <TouchableOpacity onPress={this._registrar} style={styles.button}>
+            <Text style={styles.buttonText}>REGISTRAR</Text>
+          </TouchableOpacity>
+        ) : (
+          <ActivityIndicator style={{ margin: 14 }} />
+        )}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create ({
-  container: {alignItems: 'center', padding: 6},
+const styles = StyleSheet.create({
+  container: { alignItems: "center", padding: 6 },
   row: {
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
     margin: 4,
   },
   text: {
     width: 99,
     fontSize: 10,
-    fontFamily: 'ConthraxSb-Regular',
-    color: 'black',
+    fontFamily: "ConthraxSb-Regular",
+    color: "black",
   },
   input: {
-    width: '68%',
+    width: "68%",
     height: 34,
-    backgroundColor: 'silver',
+    backgroundColor: "silver",
     borderRadius: 10,
     paddingVertical: 2,
   },
   txt1: {
     fontSize: 7,
-    fontFamily: 'ConthraxSb-Regular',
+    fontFamily: "ConthraxSb-Regular",
     width: 100,
-    color: 'black',
+    color: "black",
   },
   button: {
-    backgroundColor: '#2196F3',
-    width: '38%',
+    backgroundColor: "#2196F3",
+    width: "38%",
     paddingHorizontal: 10,
     paddingVertical: 8,
     borderRadius: 10,
@@ -291,9 +295,10 @@ const styles = StyleSheet.create ({
   },
   buttonText: {
     fontSize: 12,
-    color: 'white',
-    textAlign: 'center',
-    fontFamily: 'ConthraxSb-Regular',
+    color: "white",
+    textAlign: "center",
+    fontFamily: "ConthraxSb-Regular",
   },
-  dropdown: {width: '48%', alignItems: 'center'},
+  dropdown1: { width: "68%", alignItems: "center" },
+  dropdown2: { width: "28%", alignItems: "center" },
 });
--- src/modules/transportePub/viewTransportePub.js
+++ src/modules/transportePub/viewTransportePub.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import {
-    View,
-    Platform,
-} from 'react-native';
+import React, { Component } from "react";
+import { View, Platform } from "react-native";
 
-import Encabezado from '../../components/encabezado'
-import Busqueda from './components/busqueda'
+import Encabezado from "../../components/encabezado";
+import Busqueda from "./components/busqueda";
 
 export default class ViewTransportePub extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <Encabezado title="TRANSPORTE PÚBLICO" subtitle="GUÍA DE TRANSP. PÚBLICO" navigation={this.props.navigation}/>
-                <Busqueda />
-            </View>
-        )
-    }
-
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Encabezado
+          title="TRANSPORTE PÚBLICO"
+          subtitle="GUÍA DE TRANSP. PÚBLICO"
+          navigation={this.props.navigation}
+        />
+        <Busqueda />
+      </View>
+    );
+  }
 }
